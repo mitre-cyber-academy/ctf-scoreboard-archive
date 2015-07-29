2903bd3f31c851858d7be9ee1be21f13ce549149
Robert Clark <rbclark@mitre.org>
2015-07-29 17:07:59 -0400

    Fix devise migration.

--- db/migrate/20110816191149_devise_create_users.rb
+++ db/migrate/20110816191149_devise_create_users.rb
@@ -1,10 +1,19 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
-      t.database_authenticatable :null => false
-      t.trackable
+      t.string :email,              :null => false, :default => ""
+      t.string :encrypted_password, :null => false, :default => ""
+      t.integer  :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+      t.integer  :failed_attempts, :default => 0 # Only if lock strategy is :failed_attempts
+      t.string   :unlock_token # Only if unlock strategy is :email or :both
+      t.datetime :locked_at
+
       t.timestamps
-      t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :none
+
       # t.encryptable
       # t.confirmable
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
