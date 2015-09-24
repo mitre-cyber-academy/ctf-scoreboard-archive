32541bd5e58648817cf3b03c3646591e818543e0
Robert Clark <rbclark@mitre.org>
2015-09-24 16:33:30 -0400

    Move the code for generating user certificates into the player specific model since it is not applicable to admins.

--- app/models/player.rb
+++ app/models/player.rb
@@ -13,6 +13,8 @@ class Player < User
   geocoded_by :city
   after_validation :geocode, :if => :city_changed?
 
+  #touch file in /opt/keys
+  after_create :touch_file
 
   def score
     points = 0
@@ -23,6 +25,27 @@ class Player < User
       points = points + a.point_value
     end
     points
+  end
+
+  def display_name
+    if self.eligible
+      return read_attribute(:display_name)
+    else
+      return read_attribute(:display_name) + " (ineligible)"
+    end
+  end
+
+  # Grab the first 10 characters from the email and append the users ID.
+  # Since we have 2 different scoreboards the ID is not sufficient, therefore
+  # if we seed it with some additional data it should solve the issue.
+  def key_file_name
+    self.email.tr('^A-Za-z', '')[0..10] + self.id.to_s
+  end
+
+  private
+  
+  def touch_file
+    `touch /opt/keys/#{self.key_file_name}` 
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,27 +9,9 @@ class User < ActiveRecord::Base
   validates :email, :type, presence: true
   validates :type, inclusion: %w( Admin Player )
   
-  #touch file in /opt/keys
-  after_create :touch_file
-  
   # is admin
   def admin?
     self.is_a?(Admin)
-  end
-
-  def display_name
-    if self.eligible
-      return read_attribute(:display_name)
-    else
-      return read_attribute(:display_name) + " (ineligible)"
-    end
-  end
-
-  # Grab the first 10 characters from the email and append the users ID.
-  # Since we have 2 different scoreboards the ID is not sufficient, therefore
-  # if we seed it with some additional data it should solve the issue.
-  def key_file_name
-    self.email.tr('^A-Za-z', '')[0..10] + self.id.to_s
   end
 
   def set_password; nil; end
@@ -38,13 +20,6 @@ class User < ActiveRecord::Base
     return nil if value.blank?
     self.password = value
     self.password_confirmation = value
-  end
-  
-  
-  private
-  
-  def touch_file
-    `touch /opt/keys/#{self.key_file_name}` 
   end
   
 end
