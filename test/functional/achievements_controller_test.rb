20a99683855bdbb42719c24b7e797b3241777805
Kyle Fagan <bialogs@gmail.com>
2016-06-18 15:22:47 -0400

    Updated achievement controller test

--- test/functional/achievements_controller_test.rb
+++ test/functional/achievements_controller_test.rb
@@ -2,15 +2,9 @@ require 'test_helper'
 require 'active_record/fixtures'
 
 class AchievementsControllerTest < ActionController::TestCase
-  def setup
-  end
-
-  test 'player gets achievement' do
-    assert true
-  end
-
-  test 'name of achievement' do
-    assert_respond_to 'Achievements', :index
-    assert true
+  test 'index' do
+    # render
+    get :index
+    assert :success
   end
 end
