8de462c860a8969838d2fd8d6e481e360d940181
Kyle Fagan <bialogs@gmail.com>
2016-06-18 13:05:55 -0400

    User tests

---
+++ test/models/user_test.rb
@@ -0,0 +1,15 @@
+class UserTest < ActiveSupport::TestCase
+  test 'is an admin' do
+    assert_equal true, users(:admin_one).admin?
+  end
+
+  test 'set password' do
+    assert_nil users(:player_one).set_password
+  end
+
+  test 'set password with value' do
+    password = 'NewTestPassword123'
+    assert_nil users(:player_one).set_password=()
+    assert_equal password, users(:player_one).set_password=(password)
+  end
+end
