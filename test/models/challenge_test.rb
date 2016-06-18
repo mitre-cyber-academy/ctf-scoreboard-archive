7788bcc2a407aab7d46515e1d4be766bec070f6d
Kyle Fagan <bialogs@gmail.com>
2016-06-18 15:47:05 -0400

    WIP: Challenge model testing. Also its 4PM on a Saturday.

---
+++ test/models/challenge_test.rb
@@ -0,0 +1,18 @@
+class ChallengeTest < ActiveSupport::TestCase
+  # test 'challenge open' do
+  #   challenge = challenges(:challenge_one)
+  #   assert_equal true, challenge.challenge_open?(challenge)
+  # end
+
+  # test 'challenge closed' do
+  #   assert_equal false, challenges(:challenge_two).challenge_open?
+  # end
+
+  # test 'challenge force_closed' do
+  #   assert_equal false, challenges(:challenge_three).challenge_open?
+  # end
+
+  test 'is challenge solved' do
+    assert_equal false, challenges(:challenge_one).is_solved?
+  end
+end
