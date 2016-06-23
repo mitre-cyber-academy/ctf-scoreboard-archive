0bcb7b7c651a8048f5091d2e25aafde53e668d1b
Bryan Singh <bhsingh@mitre.org>
2016-06-23 17:04:11 -0400

    Fixed score adjustment fixture names

--- test/models/score_adjustment_test.rb
+++ test/models/score_adjustment_test.rb
@@ -4,17 +4,17 @@ class ScoreAdjustmentTest < ActiveSupport::TestCase
   include ActionView::Helpers::TextHelper
 
   test 'build description' do
-    points = feed_items(:feed_item_one).point_value
+    points = score_adjustments(:score_adjustment_one).point_value
     description_string =  %(Score was increased by <span style="color:green;">#{pluralize(points.abs, 'point')}</span>)
-    assert_equal description_string, feed_items(:feed_item_one).description
-    points = feed_items(:feed_item_two).point_value
+    assert_equal description_string, score_adjustments(:score_adjustment_one).description
+    points = score_adjustments(:score_adjustment_two).point_value
     description_string =  %(Score was decreased by <span style="color:red;">#{pluralize(points.abs, 'point')}</span>)
-    assert_equal description_string, feed_items(:feed_item_two).description
+    assert_equal description_string, score_adjustments(:score_adjustment_two).description
   end
 
   test 'build icon' do
-    assert_equal 'chevron-up', feed_items(:feed_item_one).icon
-    assert_equal 'chevron-down', feed_items(:feed_item_two).icon
+    assert_equal 'chevron-up', score_adjustments(:score_adjustment_one).icon
+    assert_equal 'chevron-down', score_adjustments(:score_adjustment_two).icon
   end
 
   test 'point value is not zero' do
