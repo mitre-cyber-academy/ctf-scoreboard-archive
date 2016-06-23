7e80ea9d6ab7661089008cdeca97558b70b77857
Bryan Singh <bhsingh@mitre.org>
2016-06-23 19:03:00 -0400

    Fixed fixtures, created tests for categories and challegnes

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,5 +2,9 @@ require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
   test 'next challenge' do
+    categories_one = categories(:binary)
+    challenge_two = challenges(:challenge_two)
+    challenge_three = challenges(:challenge_three)
+    assert_equal challenge_two, categories_one.next_challenge(challenge_three)
   end
 end
