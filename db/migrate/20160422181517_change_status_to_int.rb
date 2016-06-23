a21fee1176730c3a9d1869f820cabda477474709
Bryan Singh <bhsingh@mitre.org>
2016-06-23 18:06:49 -0400

    Fixed converting starting_state to type int

--- db/migrate/20160422181517_change_status_to_int.rb
+++ db/migrate/20160422181517_change_status_to_int.rb
@@ -1,5 +1,5 @@
 class ChangeStatusToInt < ActiveRecord::Migration
   def change
-    change_column :challenges, :starting_state, :integer
+    change_column :challenges, :starting_state, 'integer USING CAST(starting_state AS integer)'
   end
 end
