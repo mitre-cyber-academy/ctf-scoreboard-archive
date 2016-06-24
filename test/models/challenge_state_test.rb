53b4e8d7c13986d40d295ec95dcc9b06ceda800a
Bryan Singh <bhsingh@mitre.org>
2016-06-24 14:03:35 -0400

    Finished challenge state test

--- test/models/challenge_state_test.rb
+++ test/models/challenge_state_test.rb
@@ -2,5 +2,8 @@ require 'test_helper'
 
 class ChallengeStateTest < ActiveSupport::TestCase
   test 'default values' do
+    # 1 == forced-closed
+    assert_equal 'force_closed', challenge_states(:challenge_state_two_d2).default_values
+    assert_equal ChallengeState.states[:closed], challenge_states(:challenge_state_four_d2).default_values
   end
 end
