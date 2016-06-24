1056f6d4d81e52e92685df574ca310ae1ebe76af
Robert Clark <rbclark@mitre.org>
2016-06-24 14:26:40 -0400

    Cleaned up fixtures for categories and challenges.

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,9 +2,9 @@ require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
   test 'next challenge' do
-    categories_one = categories(:binary_one)
+    categories_one = categories(:category_one)
+    challenge_one = challenges(:challenge_one)
     challenge_two = challenges(:challenge_two)
-    challenge_three = challenges(:challenge_three)
-    assert_equal challenge_two, categories_one.next_challenge(challenge_three)
+    assert_equal challenge_two, categories_one.next_challenge(challenge_one)
   end
 end
