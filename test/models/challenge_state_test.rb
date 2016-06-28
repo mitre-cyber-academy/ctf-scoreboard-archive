8cdabf24cdfef394ce4cd7c4ad4ebdecd97e5188
Robert Clark <rbclark@mitre.org>
2016-06-28 12:30:52 -0400

    Update challenge tests and fixtures.

--- test/models/challenge_state_test.rb
+++ test/models/challenge_state_test.rb
@@ -4,6 +4,6 @@ class ChallengeStateTest < ActiveSupport::TestCase
   test 'default values' do
     # 1 == forced-closed
     assert_equal 'force_closed', challenge_states(:challenge_state_two_d2).default_values
-    assert_equal ChallengeState.states[:closed], challenge_states(:challenge_state_four_d2).default_values
+    assert_equal 'open', challenge_states(:challenge_state_four_d2).default_values
   end
 end
--- test/models/solved_challenge_test.rb
+++ test/models/solved_challenge_test.rb
@@ -2,6 +2,32 @@ require 'test_helper'
 
 class SolvedChallengeTest < ActiveSupport::TestCase
 
+  def setup
+    @solved_challenge_first = SolvedChallenge.new(
+      player: users(:player_one),
+      challenge: challenges(:challenge_two_cat_one),
+      flag: flags(:flag_two)
+      )
+    @solved_challenge_second = SolvedChallenge.new(
+      player: users(:player_one),
+      challenge: challenges(:challenge_one_cat_one),
+      flag: flags(:flag_one),
+      division: divisions(:division_one)
+    )
+    @solved_challenge_third = SolvedChallenge.new(
+      player: users(:player_one),
+      challenge: challenges(:challenge_four_cat_two),
+      flag: flags(:flag_four),
+      division: divisions(:division_one)
+    )
+    @solved_challenge_fourth = SolvedChallenge.new(
+      player: users(:player_two),
+      challenge: challenges(:challenge_four_cat_two),
+      flag: flags(:flag_four),
+      division: divisions(:division_two)
+    )
+  end
+
   test 'description' do
     description_string =  %(Solved challenge "#{feed_items(:solved_challenge_one).challenge.category.name} #{feed_items(:solved_challenge_one).challenge.point_value}")
     assert_equal description_string, feed_items(:solved_challenge_one).description
@@ -12,13 +38,8 @@ class SolvedChallengeTest < ActiveSupport::TestCase
   end
 
   test 'challenge is open' do
-    solved_challenge = SolvedChallenge.new(
-      player: users(:player_one),
-      challenge: challenges(:challenge_two),
-      flag: flags(:flag_two)
-      )
-    assert_not solved_challenge.valid?
-    assert_equal true, solved_challenge.errors.added?(:challenge, I18n.t('challenge.not_open'))
+    assert_not @solved_challenge_first.valid?
+    assert_equal true, @solved_challenge_first.errors.added?(:challenge, I18n.t('challenge.not_open'))
   end
 
   test 'game is open' do
@@ -26,43 +47,33 @@ class SolvedChallengeTest < ActiveSupport::TestCase
     game.stop = Time.now
     game.save
 
-    solved_challenge = SolvedChallenge.new(
-      player: users(:player_one),
-      challenge: challenges(:challenge_two),
-      flag: flags(:flag_two)
-    )
-
-    assert_not solved_challenge.valid?
-    assert_equal true, solved_challenge.errors.added?(:base, I18n.t('challenge.game_not_open'))
+    assert_not @solved_challenge_first.valid?
+    assert_equal true, @solved_challenge_first.errors.added?(:base, I18n.t('challenge.game_not_open'))
   end
 
   test 'player has already solved challenge' do
-    # solved_challenge = SolvedChallenge.new(
-    #   player: users(:player_two),
-    #   challenge: challenges(:challenge_four),
-    #   flag: flags(:flag_four),
-    #   division: divisions(:division_two)
-    # )
-
-    # assert_not solved_challenge.valid?
-    # assert_equal true, solved_challenge.errors.added?(:base, I18n.t('challenge.already_solved'))
+    assert_not @solved_challenge_fourth.valid?
+    assert_equal true, @solved_challenge_fourth.errors.added?(:base, I18n.t('challenge.already_solved'))
   end
 
   test 'award achievement' do
     assert_difference 'Achievement.count', +1 do
-      feed_items(:solved_challenge_one).award_achievement
+      feed_items(:solved_challenge_two).award_achievement
     end
   end
 
-  test 'open next challenge' do
-    solved_challenge = SolvedChallenge.new(
-      player: users(:player_one),
-      challenge: challenges(:challenge_one),
-      flag: flags(:flag_one),
-      division: divisions(:division_one)
-    )
-    solved_challenge.save!
-    solved_challenge.open_next_challenge
-    assert_equal true, challenges(:challenge_five).open?(divisions(:division_one))
+  test 'open next challenge will work when not force closed' do
+    @solved_challenge_third.save
+    @solved_challenge_third.open_next_challenge
+    assert_equal true, challenges(:challenge_five_cat_two).open?(divisions(:division_one))
+    assert_equal false, challenges(:challenge_six_cat_two).open?(divisions(:division_one))
+  end
+
+  test 'open next challenge will not open next challenge when force closed' do
+    @solved_challenge_second.save
+    @solved_challenge_second.open_next_challenge
+    assert_equal false, challenges(:challenge_two_cat_one).open?(divisions(:division_one))
+    # Next challenge will also stay closed
+    assert_equal false, challenges(:challenge_three_cat_one).open?(divisions(:division_one))
   end
 end
