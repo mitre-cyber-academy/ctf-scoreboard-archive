5fccacfe729e24d4097d1de6a5e4cd460e4a40b8
Robert Clark <rbclark@mitre.org>
2016-08-22 10:11:02 -0400

    Revert back to old flash message building since new method doesn't work with rails admin.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,17 +22,19 @@ class ApplicationController < ActionController::Base
   # rubocop:disable Metrics/AbcSize
   # rubocop:disable Metrics/CyclomaticComplexity
   # rubocop:disable Metrics/PerceivedComplexity
+  # rubocop:disable Rails/OutputSafety
   def load_game
     @game = Game.instance
     raise ActiveRecord::RecordNotFound unless @game
     unless current_user.nil?
       now = Time.zone.now
       if now < @game.start
-        flash.now[:info] = 'The game will open ' +
-                           simple_format("on #{@game.start.strftime('%b %e %y, %R %Z')}.", '', wrapper_tag: 'strong')
+        flash.now[:info] = "The game will open <strong><span id=\"major-tom\"></span> on
+        #{@game.start.strftime('%b %e %y, %R %Z')}</strong>.".html_safe
       elsif now < @game.stop && now > @game.stop - 1.hour
-        flash.now[:info] = 'The game will stop accepting submissions ' +
-                           simple_format("on #{@game.stop.strftime('%b %e %y, %R %Z')}", '', wrapper_tag: 'strong')
+        flash.now[:info] = "The game will stop accepting submissions
+        <strong><span id=\"major-tom\"></span> on #{@game.stop.strftime('%b %e %y, %R %Z')}
+        </strong>."
       else
         flash.now[:info] = I18n.t('game.closed') unless @game.open?
       end
@@ -42,6 +44,7 @@ class ApplicationController < ActionController::Base
   # rubocop:enable Metrics/AbcSize
   # rubocop:enable Metrics/CyclomaticComplexity
   # rubocop:enable Metrics/PerceivedComplexity
+  # rubocop:enable Rails/OutputSafety
 
   def load_messages_count
     unless current_user.nil?
